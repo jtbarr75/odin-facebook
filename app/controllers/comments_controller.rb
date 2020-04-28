a1ec060b5a46aa56ffb3c336c7c678de84e80761
Jason Barr <Barr75@gmail.com>
2020-04-28 11:31:18 -0700

    can leave comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:success] = "Commented!"
+      redirect_back(fallback_location: root_url)
+    else
+      render 'posts/show'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:success] = "Comment Deleted"
+    redirect_back(fallback_location: root_url)
+  end
+
 end
