e03ef97eac5a3e1876b2e4ffa38b1a4bb465f5f6
Jason Barr <Barr75@gmail.com>
2020-04-28 10:02:22 -0700

    posts can be viewed individually

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:create, :destroy]
+    resources :posts, only: [:show, :create, :destroy]
     resources :friendships, only: [:index, :create, :update, :destroy]
   end
 
-  resources :posts, only: [:index, :show] do 
+  resources :posts, only: [:index] do 
     resources :comments, only: [:create, :destroy]
   end
   
