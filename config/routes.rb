5b32d4e687184ccbf77f2cae6e3b9ac612c4b64e
Jason Barr <Barr75@gmail.com>
2020-05-04 11:43:50 -0700

    root is timeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'posts#index'
   devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:show, :create, :destroy]
