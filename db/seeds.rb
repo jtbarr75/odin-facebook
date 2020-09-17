6be259a2b27a342f5200af6a301ccaed78864726
Jason Barr <Barr75@gmail.com>
2020-09-17 14:30:01 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,16 @@ User.create!(name:  "Jason Barr",
   email = "example-#{n+1}@example.com"
   password = "password"
   User.create!(name:  name,
-               email: email,z 
+               email: email,
                password:              password,
                password_confirmation: password)
 end
 
+def content
+  return Faker::Company.bs + " and " + Faker::Company.bs
+end
+
 users = User.order(:created_at).take(6)
 10.times do
-  content = Faker::Company.bs + " and " + Faker::Company.bs
   users.each { |user| user.posts.create!(body: content) }
 end
