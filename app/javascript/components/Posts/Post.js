b94597b9afae3e38a4319065a3af6fd053ff196b
Jason Barr <Barr75@gmail.com>
2020-09-23 12:43:43 -0700

    move post edit delete into dropdown menu

--- app/javascript/components/Posts/Post.js
+++ app/javascript/components/Posts/Post.js
@@ -26,7 +26,20 @@ class Post extends React.Component {
     return (
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title"><a href={`users/${post.user.id}`}>{post.user.name}</a></h5>
+          <div className="d-flex justify-content-between">
+            <h5 className="card-title"><a href={`users/${post.user.id}`}>{post.user.name}</a></h5>
+            {this.props.currentUser.id == post.user.id && (
+              <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id={`postDropDown${post.id}`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Actions<span className="caret"></span>
+              </button>
+              <div className="dropdown-menu" aria-labelledby={`postDropDown${post.id}`}>
+                <button className="dropdown-item" href="#">Edit Post</button>
+                <button className="dropdown-item" onClick={this.handleDelete} data-id={post.id}>Delete Post</button>
+              </div>
+            </div>
+            )}
+          </div>
           <p className="card-text"><small className="text-muted">{post.createdAt}</small></p>
           <p className="card-text">{post.body}</p>
           { post.picture.url && <img className= "card-img" src={post.picture.url}/> }
@@ -38,9 +51,6 @@ class Post extends React.Component {
         <div className="card-footer d-flex justify-content-between">
           <button className="btn btn-primary">Like</button>
           <button className="btn btn-primary">Comment</button>
-          {this.props.currentUser.id == post.user.id && 
-            <button onClick={this.handleDelete} data-id={post.id} className="btn btn-primary" >Delete</button>
-          }
         </div>
       </div>
     )
