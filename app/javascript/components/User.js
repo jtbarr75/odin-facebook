3c407e591618f80d98f0a12527f957c46d36c92a
Jason Barr <Barr75@gmail.com>
2020-10-01 11:28:18 -0700

    style user page

--- app/javascript/components/User.js
+++ app/javascript/components/User.js
@@ -4,13 +4,17 @@ import Posts from './Posts/Posts'
 class User extends React.Component {
   render () {
     const { user, currentUser } = this.props
+    console.log(user)
     let edit;
-    if (currentUser.id == user.i) {
+    let friendOptions;
+    if (currentUser.id == user.id) {
       edit = (
         <p><a href="#" className="btn btn-xs btn-primary">Edit Profile</a></p>
       )
+    } else {
+      friendOptions = <p>Not Friends yet</p>;
     }
-    let friendOptions = <p>Not Friends yet</p>;
+    
     // if (user.friends.includes(currentUser.id)) {
     //   friendOptions = <p><span className="label label-success">Friends!</span></p>
     // } else if (currentUser.notifications.sentFriendRequests.includes(user.id)) {
@@ -24,12 +28,20 @@ class User extends React.Component {
     return (
       <Fragment>
         <div className="container">
-          <h2 align="left ">{user.name}</h2>
-          {/* <img href={user.picture && user.picture.url} > */}
-          <p><a href="#">{user.friends.length} Friends</a></p>
-          <p>Email: { user.email }</p>
-          {edit}
-          {friendOptions}
+          <div className="row justify-content-center">
+            <div className="col-8 justify-content-center">
+              <div className="media align-items-center mt-3">
+                <img className="mr-3" src={user.picture && user.picture.url}/>
+                <div className="media-body">
+                  <h2>{user.name}</h2>
+                  <p><a href="#">{user.friends.length} Friends</a></p>
+                  {edit}
+                  {friendOptions}
+                </div>
+              </div>
+            </div>
+          </div>
+          
           <Posts currentUser={currentUser} posts={this.props.user.posts}/>
         </div>
       </Fragment>
